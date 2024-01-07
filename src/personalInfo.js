361fe222696e330e9066dc00fde2ffdf39e5464a
vipin kumar vishwakarma <vishwakarmavipin3434@gmail.com>
2024-01-07 15:51:12 +0530

    project done

--- src/personalInfo.js
+++ src/personalInfo.js
@@ -1,49 +1,40 @@
 const personalInfo = [
-    {
-        id: 1,
-        name: "Akash Singh Marko",
-        img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
-        githubLink: "https://github.com/akashsinghmarko",
-        instaLink: "https://www.instagram.com/_mr.akash_singh_21/",
-        linkedinLink: "",
-        twitterLink: ""
-    },
-    {
-        id: 2,
-        name: "Nilesh Singh",
-        img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
-        githubLink: "",
-        instaLink: "",
-        linkedinLink: "",
-        twitterLink: ""
-    },
-    {
-        id: 3,
-        name: "Udit Jhariya",
-        img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
-        githubLink: "",
-        instaLink: "",
-        linkedinLink: "",
-        twitterLink: ""
-    },
-    {
-        id: 4,
-        name: "Vipin Vishwakarma",
-        img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
-        githubLink: "",
-        instaLink: "",
-        linkedinLink: "",
-        twitterLink: ""
-    },
-    {
-        id: 5,
-        name: "Vipin Vishwakarma",
-        img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
-        githubLink: "",
-        instaLink: "",
-        linkedinLink: "",
-        twitterLink: ""
-    }
+  {
+    id: 1,
+    name: "Akash Singh Marko",
+    img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
+    githubLink: "https://github.com/akashsinghmarko",
+    instaLink: "https://www.instagram.com/_mr.akash_singh_21/",
+    linkedinLink: "",
+    twitterLink: "",
+  },
+  {
+    id: 2,
+    name: "Nilesh Singh",
+    img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
+    githubLink: "https://github.com/nilesh1333/",
+    instaLink: "",
+    linkedinLink: "",
+    twitterLink: "",
+  },
+  {
+    id: 3,
+    name: "Udit Jhariya",
+    img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
+    githubLink: "",
+    instaLink: "",
+    linkedinLink: "",
+    twitterLink: "",
+  },
+  {
+    id: 4,
+    name: "Vipin Vishwakarma",
+    img: "https://imgs.search.brave.com/hwJgEdHY-1clDMkLuT4ZZSG8zX81fxob_3VIyLQMPGc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucm9ja2V0cmVh/Y2guY28vaW1hZ2Vz/L3Byb2ZpbGVfcGlj/cy92Mi4xL2RwcG00/LnBuZw",
+    githubLink: "https://github.com/vipinishere",
+    instaLink: "",
+    linkedinLink: "",
+    twitterLink: "",
+  },
 ];
 
 export default personalInfo;
